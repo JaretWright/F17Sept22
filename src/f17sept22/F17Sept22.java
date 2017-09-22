714683c5daa31e04a0d40be6ab81e25d52a374b6
JaretWright <hkkA223>
2017-09-22 09:04:51 -0400

    Added phone validation

--- src/f17sept22/F17Sept22.java
+++ src/f17sept22/F17Sept22.java
@@ -12,16 +12,24 @@ public class F17Sept22 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Please enter your area code: ");
+        System.out.print("Please enter your area code or phone number: ");
         Scanner keyboard = new Scanner(System.in);
         String userInput = keyboard.nextLine();
         
         System.out.printf("That is a%s area code%n", checkAreaCode(userInput)?
                                                     " valid" : "n invalid");
+        System.out.printf("That is a%s phone number%n", checkPhoneNumber(userInput)?
+                                                    " valid" : "n invalid");
+
     }  
     
     public static boolean checkAreaCode(String areaCode)
     {
         return areaCode.matches("[2-9]\\d{2}");
+    }
+    
+    public static boolean checkPhoneNumber(String phoneNum)
+    {
+        return phoneNum.matches("\\(?[2-9]\\d{2}\\)?[-.\\s]?[2-9]\\d{2}[-.\\s]?\\d{4}");
     }
 }
